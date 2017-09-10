e97a39e62c150b0b481d7d48eada9fa6d2792cb3
html5css3-pro <html5css3.pro@gmail.com>
2017-09-11 00:27:51 +0300

    update

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,6 +1,66 @@
 $(document).ready(function() {
-	$('.click-icon').click(function() {
-		alert('Все получилось!');
-		return false;
+
+	$('#my_video').prop('muted', true);
+
+	$('#my_video').click( function (){
+	    if($(this).prop('muted')) {
+		        $(this).prop('muted', false);
+		        $(this).addClass('active');
+		        $('.app').addClass('active');
+		        $('.navigation').removeClass('active');
+		        $('.app section').removeClass('active');
+			} else {
+		    	$(this).prop('muted', true);
+		    	$(this).removeClass('active');
+		    	$('.app').removeClass('active');
+		    	$('.navigation').addClass('active');
+		}
+	});
+
+	$('.navigation a').click(function(e) {
+		e.preventDefault();
+	});
+
+	$('.l-serv').click(function() {
+		$('.service').addClass('active');
+	});
+	$('.l-about').click(function() {
+		$('.about').addClass('active');
+	});
+	$('.l-contact').click(function() {
+		$('.contact').addClass('active');
+	});
+	$('.l-clients').click(function() {
+		$('.clients').addClass('active');
+	});
+
+	$('.close').click(function() {
+		$('.app > section').removeClass('active');
+	});
+
+	setTimeout(function() {
+		$('.navigation').addClass('active');
+	}, 2000);
+
+	var titles = [
+	    "whitechurch",
+	    "and",
+	    "begin",
+	    "love",
+	    "true",
+	    "development",
+	    "resolution",
+	    "something",
+	    "more",
+    ];
+    
+    setInterval(function() {
+        var i = Math.round((Math.random()) * titles.length);
+        if (i == titles.length) --i;
+        $(".js-title").html(titles[i]);
+    }, 500);
+
+    $('.mobile_menu').click(function() {
+		$('.app').toggleClass('show_mobile');
 	});
 });
